2198b661ca7b0fb28238df115340b9bdd0ba9065
Jan Walther <mail@janericwalther.de>
2023-05-04 12:11:12 +0200

    Intro decoding (#3)
    * assignemts 37 & 38 done
    
    * cleanup
    
    * cleanup

--- src/main.ts
+++ src/main.ts
@@ -40,12 +40,17 @@ enum Opcode {
     mov = 0b0100010,
 }
 
+const input_file = Deno.args[0];
+const in_file = Deno.openSync(input_file);
+const out_file = Deno.openSync('out.asm', { write: true, create: true, truncate: true});
+const encoder = new TextEncoder();
+const debug = false;
 
 function main() {
     const in_buff = new Uint8Array(1024);
     const bytesRead = in_file.readSync(in_buff) || 0;
-    write_line(`; ${input_file}\n`);
-    write_line('bits 16\n');
+    write_line(`; ${input_file} dissassembly:`);
+    write_line('bits 16');
 
     for (let i = 0; i < bytesRead; i++) {
         const op_code = in_buff[i] >> 2;
@@ -56,19 +61,23 @@ function main() {
         const reg = in_buff[i] >> 3 & 0b0111;
         const r_m = in_buff[i] & 0b0111;
         
-        bin_print('BYTE1', in_buff[0]);
-        bin_print('BYTE2', in_buff[1]);
-        bin_print('OPCODE', op_code);
-        bin_print('D', d);
-        bin_print('W', w);
-        bin_print('MOD', mod);
-        bin_print('REG', reg);
-        bin_print('R/M', r_m);
-        if (op_code === Opcode.mov)
-        write_line(parse_mov(d, w, mod, reg, r_m));
+        debug_print('BYTE1', in_buff[0]);
+        debug_print('BYTE2', in_buff[1]);
+        debug_print('OPCODE', op_code);
+        debug_print('D', d);
+        debug_print('W', w);
+        debug_print('MOD', mod);
+        debug_print('REG', reg);
+        debug_print('R/M', r_m);
+        
+        switch (op_code) {
+        case Opcode.mov:
+            write_line(parse_mov(d, w, mod, reg, r_m));
+            break;
+        default:
+            console.error('Unknown Opcode.');
+        }
     }
-    
-    
     
     in_file.close();
     out_file.close();
@@ -81,25 +90,20 @@ function write_line(s: string) {
 
 function parse_mov(d: number, w: number, mod: number, reg: number, r_m: number): string{
     w = w << 3;
-    
     let src = reg_table[w | reg];
     let dest = reg_table[w | r_m];
 
-    if (d === 0b1) {
+    if (d) {
         [src, dest] = [dest, src];
     }
     
-    console.log(dest, src);
+    if (debug) console.log('mov', dest, src);
     return `mov ${dest}, ${src}`;
 }
 
-function bin_print(name: string, bytes: number) {
-    console.log(name.padStart(8), (bytes >>> 0).toString(2).padStart(8, '0'));
+function debug_print(name: string, bytes: number) {
+    if (debug)
+        console.log(name.padStart(8), (bytes >>> 0).toString(2).padStart(8, '0'));
 }
-
-const input_file = Deno.args[0];
-const in_file = Deno.openSync(input_file);
-const out_file = Deno.openSync('out.asm', { write: true, create: true, truncate: true});
-const encoder = new TextEncoder();
 
 main();
